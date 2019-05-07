0fec0cbd3a813198d45f10ed42e9590ffee1374f
Henry Ramirez <contactoraygan@gmail.com>
2019-05-07 12:15:55 -0500

    Display data on modal vue

--- index.php
+++ index.php
@@ -20,7 +20,10 @@
                     </div>
                     <div class="search-box">
                         <input v-model="query" type="text" class="search-box__input">
-                        <button v-on:click="search" class="button button-primary search-box__button">
+                        <button 
+                            v-on:click="search" 
+                            class="button button-primary search-box__button"
+                            :disabled="this.query.length < 3">
                             {{ button_text }}
                         </button>
                     </div>
@@ -45,7 +48,7 @@
                         :key="key" 
                         class="repo">
                         <div class="repo__header">
-                            <p v-on:click="openModal(item.full_name)" class="repo__title">
+                            <p v-on:click="openModal(item)" class="repo__title">
                                 {{item.full_name}}
                             </p>
                             <p class="repo__stars">
@@ -59,7 +62,54 @@
                     </div>
                     <modal v-if="showModal" @close="showModal = false">
                         <div slot="body">
-                            {{ title }}
+                            <div class="repo-modal-header">
+                                <h2 class="repo-modal-title">
+                                 {{ modal_info.name }}
+                                </h2>
+                                <p class="repo-modal-fullname">
+                                {{ modal_info.description }}
+                                </p>
+                            </div>
+                            <div class="author">
+                                <table>
+                                    <tr>
+                                        <td class="author-key">Author</td>
+                                        <td class="author-info">
+                                            <img class="author-img" :src="modal_info.owner.avatar_url" :alt="modal_info.owner.login">
+                                            {{modal_info.owner.login}}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="author-key">Full name</td>
+                                        <td class="author-info">{{modal_info.full_name}}</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="author-key">Created</td>
+                                        <td class="author-info">
+                                            {{ dateFormatter(modal_info.created_at) }}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="author-key">License</td>
+                                        <td class="author-info">
+                                            <span v-if="modal_info.license !== null">
+                                                {{ modal_info.license.name }}
+                                            </span>
+                                            <span v-else>
+                                                There is no information about the license
+                                            </span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="author-key">Clone</td>
+                                        <td class="author-info">
+                                            <span>
+                                                git clone {{ modal_info.clone_url }}
+                                            </span>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
                         </div>
                     </modal>
                 </div>
@@ -75,7 +125,7 @@
                         <div class="modal-footer">
                             <slot name="footer">
                                 <button class="modal-default-button" @click="$emit('close')">
-                                    Cerrar
+                                    <i class="fas fa-times"></i>
                                 </button>
                             </slot>
                         </div>
--- vue/main.js
+++ vue/main.js
@@ -10,32 +10,17 @@ var app = new Vue({
 		results_size: 0,
 		results: [],
 		isSearch: false,
-		showModal: false
+		showModal: false,
+		modal_info: Object,
+		actual_commits: {}
 	},
 	methods: {
 		search() {
-			this.isSearch = true;
-			this.results = [];
-			this.results_size = 0;
+			this.resetSearch();
 			fetch(`https://api.github.com/search/repositories?q=${this.query}`)
 				.then((data) => data.json())
 				.then((response) => {
-					fetch('/github-search/api/log.php', {
-						headers: {
-							Accept: 'application/json',
-							'Content-Type': 'application/json'
-						},
-						method: 'POST',
-						body: JSON.stringify({ keyword: this.query })
-					}).then((res) =>
-						res.json().then((response) => {
-							console.log(response);
-						})
-					);
-
-					this.isSearch = false;
-					this.results = response.items;
-					this.results_size = response.total_count;
+					this.completeSearch(response, true);
 				});
 		},
 		kFormatter(num) {
@@ -43,12 +28,64 @@ var app = new Vue({
 				? Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + 'k'
 				: Math.sign(num) * Math.abs(num);
 		},
-		openModal(text) {
+		dateFormatter(str) {
+			if (!str) {
+				return '(n/a)';
+			}
+			str = new Date(str);
+			return (
+				str.getFullYear() +
+				'-' +
+				(str.getMonth() < 9 ? '0' : '') +
+				(str.getMonth() + 1) +
+				'-' +
+				(str.getDate() < 10 ? '0' : '') +
+				str.getDate()
+			);
+		},
+		openModal(item) {
+			console.log(item);
 			this.showModal = true;
-			this.title = text;
+			this.modal_info = item;
+			this.getListCommits(this.modal_info.default_branch);
 		},
 		closeModal() {
+			this.modal_info = {};
 			this.showModal = false;
+		},
+		savedSearch(query) {
+			fetch('/github-search/api/log.php', {
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json'
+				},
+				method: 'POST',
+				body: JSON.stringify({ keyword: query })
+			}).then((res) =>
+				res.json().then((response) => {
+					// console.log(response);
+				})
+			);
+		},
+		getListCommits(default_branch) {
+			const url = `https://api.github.com/repos/vuejs/vue/commits/${default_branch}`;
+			fetch(url)
+				.then((data) => data.json())
+				.then((response) => {
+					console.log(response);
+			});
+		},
+		resetSearch() {
+			this.isSearch = true;
+			this.results = [];
+			this.results_size = 0;
+			this.modal_info = {};
+		},
+		completeSearch(response, status) {
+			this.isSearch = !status;
+			this.results = response.items;
+			this.results_size = response.total_count;
+			this.savedSearch(this.query);
 		}
 	}
 });

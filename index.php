bab4f9e5ed3dc25d78aecff3b53638de2fb6e502
Henry Ramirez <contactoraygan@gmail.com>
2019-05-07 04:08:20 -0500

    Vue Search and Render from Query

--- index.php
+++ index.php
@@ -5,49 +5,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="assets/master.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <title>GitHub Search</title>
     <script src="https://cdn.jsdelivr.net/npm/vue"></script>
     
 </head>
 <body>
-    <header id="search" class="main-header">
-        <div class="container">
-            <div class="search-component">
-                <div class="search-component__brand">
-                    <img src="http://www.logospng.com/images/149/tools-for-information-literacy-welcome-to-inls161-149487.png" alt="GitHub">
-                </div>
-                <div class="search-box">
-                    <input v-model="query" type="text" class="search-box__input">
-                    <button v-on:click="search" class="button button-primary search-box__button">
-                        {{ button_text }}
-                    </button>
+
+    <div id="search">
+        <header class="main-header">
+            <div class="container">
+                <div class="search-component">
+                    <div class="search-component__brand">
+                        <img src="http://www.logospng.com/images/149/tools-for-information-literacy-welcome-to-inls161-149487.png" alt="GitHub">
+                    </div>
+                    <div class="search-box">
+                        <input v-model="query" type="text" class="search-box__input">
+                        <button v-on:click="search" class="button button-primary search-box__button">
+                            {{ button_text }}
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-    </header>
+        </header>
 
-    <div class="container">
-        <section class="list-repositories">
-            <h3 class="list-repositories__stats">
-                530,671 repository results
-            </h3>
-            <!--stargazers_count-->
-            <div class="repos">
-                <div class="repo">
-                    <div class="repo__header">
-                        <p class="repo__title">
-                            surmon-china/vue-awesome-swiper
-                        </p>
-                        <p class="repo__stars">
-                            
+        <div class="container">
+            <section class="list-repositories">
+                <h3 v-if="results.length > 0" class="list-repositories__stats">
+                    {{ results_size }} repository results
+                </h3>
+                <!--stargazers_count-->
+                <div v-if="results.length > 0" class="repos">
+                    <div v-for="(item, key) in results" :key="key" class="repo">
+                        <div class="repo__header">
+                            <p class="repo__title">
+                                {{item.full_name}}
+                            </p>
+                            <p class="repo__stars">
+                                <i class="fas fa-star"></i>
+                                {{kFormatter(item.stargazers_count)}}
+                            </p>
+                        </div> 
+                        <p class="repo__description">
+                            {{item.description}}
                         </p>
                     </div>
-                    <p class="repo__description">
-                        🏆 Swiper component for @vuejs
-                    </p>
                 </div>
-            </div>
-        </section>
+            </section>
+        </div>
     </div>
     <script src="vue/main.js"></script>
 </body>
--- vue/main.js
+++ vue/main.js
@@ -2,17 +2,23 @@ var app = new Vue({
 	el: '#search',
 	data: {
 		button_text: 'Buscar',
-		query: ''
+		query: '',
+		results_size: 0,
+		results: []
 	},
 	methods: {
 		search() {
-			fetch('http://localhost/SimpleDemoPHP/api')
-				.then(function(response) {
-					return response.json();
-				})
-				.then(function(myJson) {
-					console.log(myJson);
+			fetch(`https://api.github.com/search/repositories?q=${this.query}`)
+				.then((data) => data.json())
+				.then((response) =>  {
+					this.results = response.items;
+					console.log(response);
+					this.results_size = response.total_count;
+					console.log(this.results);
 				});
+		},
+		kFormatter(num) {
+			return Math.abs(num) > 999 ? Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) + 'k' : Math.sign(num)*Math.abs(num)
 		}
 	}
 });
